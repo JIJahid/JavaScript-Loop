7ef8e2782705c2d75c1fe482b4dd9f92823b266e
J.I. Jahid <j.i.jahid90@gmail.com>
2025-10-16 23:19:55 +0600

    loopss added

---
+++ loop.js
@@ -0,0 +1,17 @@
+
+let number = [25, 22, 65, 90, 44, 80, 55]
+for (i = 0; i <= number.length-1; i++){
+    if (number[i] < 70){
+        console.log(number[i] + 20);
+    }
+    else if(number[i] >= 80 && number[i] < 90){
+        console.log(number[i] + 5);
+    }
+    else{
+         console.log(number[i]);
+    }
+}
+
+/* for each variable 20 is added to values below 70 & 5 is added to values queal to 80 & less than 90. All the latest values are printed */
+
+/* for each variable 20 is added to values below 70 & 5 is added to values queal to 80 & less than 90. All the latest values are printed */
